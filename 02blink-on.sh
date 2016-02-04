98553d4d9f4759f3a9ff22b1f01a18dbb9249a6a
dmgeurts <d.m.geurts@geurtscass.com>
2016-02-04 22:34:31 +0000

    Modified LEDs for WD01
    # /usr/sbin/pioctl       
    
    Usage: /usr/sbin/pioctl type cmd
       type: satus|fan|sata|wifi|internet
       cmd : 
           : status: 0: LED ON, 1: LED OFF, 2:LED Wink, 3: LED Wink Stop
           : fan   : 0: FAN STOP, 1: FAN 1, 2: FAN 2
           : sata  : 0: SATA0 LED ON, 1: SATA0 LED OFF, 2: SATA1 LED ON, 3: SATA1 LED OFF
           :         4: SATA0 FLED ON, 5: SATA0 FLED OFF, 6: SATA1 FLED ON, 7: SATA1 FLED OFF
           : usb   : 0: USB1 ON, 1: USB1 OFF, 2: USB1 WINK START, 3: USB1 WINK STOP
                     4: USB2 ON, 5: USB2 OFF, 6: USB2 WINK START, 7: USB2 WINK STOP
           : beep  <sec>: the period for beep
           : power <off|on>
           **: wifi: 0: LED ON, 1: LED OFF, 2:LED Wink, 3: LED Wink Stop
           : internet: 0: BLUE LED ON, 1: BLUE LED OFF, 2: LED Wink, 3: LED Wink Stop,** 4:RED LED ON, 5: RED LED OFF

--- 02blink-on.sh
+++ 02blink-on.sh
@@ -1,6 +1,8 @@
 # start blinking LEDs
+# WD01: internet and wifi work, RW and battery status LEDs not blinking
+# 
 /usr/sbin/pioctl internet 2
 sleep 0.2
-/usr/sbin/pioctl status 2
-sleep 0.2
+#/usr/sbin/pioctl status 2
+#sleep 0.2
 /usr/sbin/pioctl wifi 2
