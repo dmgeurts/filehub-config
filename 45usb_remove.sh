cdffa8263405ff1ba7630618239a7396fa79f524
dmgeurts <d.m.geurts@geurtscass.com>
2016-02-04 23:37:10 +0000

    Create 45usb_remove.sh
    Copied over from fbartels

---
+++ 45usb_remove.sh
@@ -0,0 +1,32 @@
+# Kill rsync when a USB drive is removed
+
+# Delete existing modification from file, if it exists
+sed -i '/#START_MOD/,/#END_MOD/d' /etc/udev/script/remove_usb_storage.sh
+
+# Add call to usb backup script after drive mounts
+cat <<'EOF' >> /etc/udev/script/remove_usb_storage.sh 
+#START_MOD
+# Kill the rsync process if the USB drive or SD card is removed
+if [ -e /tmp/backup.pid ]; then
+        kill $(cat /tmp/backup.pid)
+        killall rsync
+        rm /tmp/backup.pid
+fi
+# Turn off swap if the store drive is removed
+STORE_DIR=.vst
+# Check if a USB drive is attached which is initialize for storing monitoring data
+check_storedrive() {
+        while read device mountpoint fstype remainder; do
+        if [ ${device:0:7} == "/dev/sd" -a -e "$mountpoint/$STORE_DIR"/rsync ];then
+                return 1
+        fi
+        done < /proc/mounts
+        return 0
+}
+# If the store drive is no longer attached, turn off swap
+check_storedrive
+if [ $? -eq 0 ]; then
+    swapoff "$mountpoint/$STORE_DIR"/swapfile
+fi
+#END_MOD
+EOF
